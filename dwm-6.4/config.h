127880f59f578a579fa3fc210b9e9d73bc2ff59b
DAnaselo <Abukaffanas5@gmail.com>
2023-11-27 19:16:54 +0200

    General Fixes

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=24" };
-static const char dmenufont[]       = "monospace:size=24";
+static const char *fonts[]          = { "Noto Nerd Font:size=10" };
+static const char dmenufont[]       = "Noto Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -84,7 +84,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_e,      spawn,          SHCMD("dolphin")},
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("thunar")},
 	{ MODKEY,                       XK_g,      spawn,          SHCMD("com.google.Chrome")},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
