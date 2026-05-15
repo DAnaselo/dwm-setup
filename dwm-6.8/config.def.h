cfc46017cabeccf647753979d0a29aa1c8df4036
DAnaselo <Abukaffanas5@gmail.com>
2026-05-15 12:49:19 +0300

    startup command for mouse acceleration

--- dwm-6.8/config.def.h
+++ dwm-6.8/config.def.h
@@ -34,6 +34,7 @@ static const char *colors[][3]      = {
 
 static const char *const autostart[] = {
   "xset", "r", "rate", "350", "50", NULL,
+  "xinput", "set-prop", "7", "libinput Accel Profile Enabled", "0", "1", "0", NULL,
 	"nitrogen", "--restore", NULL,
   "slstatus", NULL,
 	NULL /* terminate */
