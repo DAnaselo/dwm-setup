47b210ea6e1437ed5237c88d92dc3a6d49a145b8
DAnaselo <Abukaffanas5@gmail.com>
2026-05-15 18:09:58 +0300

    added volume controls

--- dwm-6.8/config.def.h
+++ dwm-6.8/config.def.h
@@ -1,3 +1,5 @@
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -75,15 +77,18 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[]   = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_orange, "-sf", col_gray4, NULL };
+static const char *termcmd[]    = { "st", NULL };
+static const char *mutecmd[]    = { "wpctl", "set-mute", "@DEFAULT_AUDIO_SINK@", "toggle", NULL };
+static const char *volupcmd[]   = { "wpctl", "set-volume", "-l", "1.0", "@DEFAULT_AUDIO_SINK@", "2%+", NULL };
+static const char *voldowncmd[] = { "wpctl", "set-volume", "-l", "1.0", "@DEFAULT_AUDIO_SINK@", "2%-", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ Mod1Mask,                     XK_space,  spawn,          {.v = dmenucmd } },
 	{ Mod1Mask,                     XK_Return, spawn,          {.v = termcmd } },
-    { MODKEY,                       XK_w,      spawn,          SHCMD("xdg-open https://")},
-    { MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("slock")},
+  { MODKEY,                       XK_w,      spawn,          SHCMD("xdg-open https://")},
+  { MODKEY|ShiftMask,             XK_l,      spawn,          SHCMD("slock")},
 	{ MODKEY,                       XK_e,      spawn,          SHCMD("pcmanfm")},
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
@@ -112,6 +117,9 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
 	TAGKEYS(                        XK_5,                      4)
+  { 0,                            XF86XK_AudioMute,        spawn, {.v = mutecmd } },
+  { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+  { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
 };
 
 /* button definitions */
